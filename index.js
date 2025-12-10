1617352e123b3cb3d465174b564f4689c92c50c0
Carlos Andrade <andradecarlos2503@hotmail.com>
2025-12-10 08:11:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const yesBtn = document.querySelector('#yesBtn');
 
 // Comportamiento del botón "Sí"
 yesBtn.addEventListener('click', function () {
-    alert('haber si esto te alegra un poco'); // Puedes mandar los mensajes que quieras
+    alert('De nada'); // Puedes mandar los mensajes que quieras
     location.href = 'https://music.youtube.com/watch?v=yMV8GemAhuA&si=ZGn9G-dfbTl2fV_w'; // Cambiar el URL en YouTube de la canción que quieras
 });
 
